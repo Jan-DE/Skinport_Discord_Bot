e14219a0a3226e949bcc932501e419d7168b4714
Jan-DE <JanNotorious@gmail.com>
2023-01-17 21:14:25 +0100

    Removed unused destructured assignment

--- DiscordBot.js
+++ DiscordBot.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, EmbedBuilder, Embed, ActivityType } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder, ActivityType } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const StringBuilder = require('string-builder');
 const numberFormatter = new Intl.NumberFormat('en-US');
